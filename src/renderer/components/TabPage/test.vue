e8c5c8e7e4adeb8e1f1c39ff17da3d5c28eebe79
kritchakorn <thipbococo@gmail.com>
2017-10-06 13:43:40 +0700

    no message

---
+++ src/renderer/components/TabPage/test.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    tab2
+    <router-link class="button is-info" to="/">Go to home</router-link>
+    <a @click="debug()">tst</a>
+    <a class="button" @click="onCheckAction()">Check {{i}}</a>
+    <a class="button" @click="onCheckAction2()">Start</a>
+    <a class="button" @click="onCheckAction3()">Stop</a>
+    <a class="button" @click="onCheckAction4()">write file</a>
+    <a class="button" @click="onCheckAction5()">read file</a>
+    <a class="button" @click="onCheckAction6()">global</a>
+  </div>
+</template>
+
+<script>
+import jbossutil from '@/helper/jbossutil'
+import Common from '@/helper/Common'
+export default {
+
+  mounted () {
+    console.log('test')
+  },
+  data () {
+    return {
+      lists: [],
+      username: '',
+      password: '',
+      i: 'none'}
+  },
+  components: {
+  },
+  methods: {
+    debug () {
+      console.log('dd')
+    },
+    onCheckAction () {
+      this.i = jbossutil.checkstatus('10.250.3.36', 'root', 'password')
+    },
+    onCheckAction2 () {
+      console.log(jbossutil.startjboss('10.250.3.36', 'root', 'password'))
+    },
+    onCheckAction3 () {
+      console.log(jbossutil.stopjboss('10.250.3.36', 'root', 'password'))
+    },
+    onCheckAction4 () {
+      console.log(Common.writefileconf())
+    },
+    onCheckAction5 () {
+      console.log(Common.readfileconf())
+    },
+    onCheckAction6 () {
+      console.log(jbossutil.echo())
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ src/renderer/helper/config.js
@@ -0,0 +1,10 @@
+var config = {}
+config.JBOSS_HOME = '/home/jboss/jboss-4.2.3.GA/'
+config.SSH_DEFAULT = '10.250.3.36'
+config.SSH_USER = 'bemhq'
+config.SSH_PASS = '@HQbem246'
+config.BEMHQSCTIP = '/home/bemhq/script/'
+config.SOURCE = '/home/bemhq/deployfile_'
+config.DESC = '/home/jboss/jboss-4.2.3.GA/server/default/deploy/'
+
+module.exports = config
